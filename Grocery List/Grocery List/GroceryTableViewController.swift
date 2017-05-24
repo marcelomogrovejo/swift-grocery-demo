2d10a184d4a9451b28592efcd2a04e532b990601
Marcelo Mogrovejo <mmogrovejo@mmogrovejo.local>
2017-05-24 14:28:27 -0300

    Some details

--- Grocery List/Grocery List/GroceryTableViewController.swift
+++ Grocery List/Grocery List/GroceryTableViewController.swift
@@ -18,6 +18,7 @@ class GroceryTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Get reference to app delegate
 //        let appDelegate = UIApplication.shared.delegate as! AppDelegate
 //        managedObjectContext = appDelegate.persistentContainer.viewContext
         
@@ -54,17 +55,17 @@ class GroceryTableViewController: UITableViewController {
             textField.autocapitalizationType = UITextAutocapitalizationType.words
         }
         
-        let addAction = UIAlertAction(title: "Add", style: .default) { (alertAction: UIAlertAction) in
+        let addAction = UIAlertAction(title: "Add", style: .default) { [weak self] (alertAction: UIAlertAction) in
             
             if let itemString = alertController.textFields?.first?.text {
-                let grocery = Grocery(context: self.managedObjectContext!)
+                let grocery = Grocery(context: (self?.managedObjectContext!)!)
                 grocery.item = itemString
                 
                 // Save the new item on core data
                 do {
-                    try self.managedObjectContext?.save()
+                    try self?.managedObjectContext?.save()
                     
-                    self.loadData()
+                    self?.loadData()
                 } catch {
                     fatalError("Error in storing data")
                 }
@@ -99,50 +100,5 @@ class GroceryTableViewController: UITableViewController {
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
